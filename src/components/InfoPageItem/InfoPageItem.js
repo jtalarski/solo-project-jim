2211fa21bfab746aad52abc477e665780e35ca9e
Jim Talarski <jim.talarski@gmail.com>
2020-10-20 04:10:17 -0500

    2002 hooked up InfoPageItem.js

--- src/components/InfoPageItem/InfoPageItem.js
+++ src/components/InfoPageItem/InfoPageItem.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+//import mapStoreToProps from '../../redux/mapStoreToProps';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
 // the component name TemplateClass with the name for the new
 // component.
-class TemplateClass extends Component {
+class InfoPageItem extends Component {
   state = {
-    heading: 'Class Component',
+    heading: 'InfoPageItem',
   };
 
   render() {
     return (
       <div>
-        <h2>{this.state.heading}</h2>
+        <pre>{JSON.stringify(this.props.search)}</pre>
       </div>
     );
   }
 }
-
-export default connect(mapStoreToProps)(TemplateClass);
+const mapStateToProps = reduxState => ({
+    queue: reduxState.queue,
+    user: reduxState.user,
+    watched: reduxState.watched,
+    search: reduxState.search
+  });
+export default (InfoPageItem);
